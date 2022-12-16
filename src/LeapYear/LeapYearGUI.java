a62cafc41229ba8fac542eb361f7f0bfc29d9e7f
jasperket <jasperkeith1.vallecera@gmail.com>
2022-12-16 18:16:30 +0800

    Add LeapYear main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -8,6 +8,11 @@ public class LeapYearGUI extends JFrame {
     private JButton btnCheckYear;
 
     public static void main(String[] args) {
-
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.panel1);
+        app.setSize(600,600);
+        app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        app.setTitle("Leap Year Checker");
+        app.setVisible(true);
     }
 }
